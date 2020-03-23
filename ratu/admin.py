817e90ba5ccc7067d13d3b426be35fae6f2033b2
Iva-khar <ivakhar@gmail.com>
2020-03-23 16:33:41 +0200

    1

--- ratu/admin.py
+++ ratu/admin.py
@@ -1,28 +1,3 @@
 from django.contrib import admin
-import xmltodict
-import sys, codecs
+
 # Register your models here.
-from .models import Choice, Question
-from .models import Region
-# from django.utils import timezone
-Question.objects.all().delete()
-
-with codecs.open('/home/ivan/Project/Django/data_converter/ratu_test.xml', encoding="cp1251") as file:
-    data = xmltodict.parse(file.read()) 
-
-print (data['DATA']['RECORD'][10])
-
-
-for x in data['DATA']:
-
-    region = Region(name=x['OBL_NAME'])
-   # region.name = 
-    region.save()
-
-
-q = Question(question_text="What view?")
-# delete
-q.save()
-print(q.question_text)
-print(q.id)
-
