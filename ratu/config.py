281cd77134e354fa61aee4a6298f667566b0f2ba
Iva-khar <ivakhar@gmail.com>
2020-03-23 16:36:05 +0200

    2

--- ratu/config.py
+++ ratu/config.py
@@ -1,3 +1,11 @@
 FILE_URL = "https://data.gov.ua/dataset/75e57837-128b-49e1-a007-5e7dfa7bf6af/resource/e21a1e57-051c-46ea-9c8e-8f30de7d863d/download/28-ex_xml_atu.zip"
-LOCAL_FILE_NAME = "28-ex_xml_atu.xml"
+LOCAL_FILE_NAME = "ratu_test1.xml"
 LOCAL_FOLDER = "unzipped_xml/"
+DATABASE_CONFIG = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gov_data',
+        'USER': 'postgre',
+        'PASSWORD': '555',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
