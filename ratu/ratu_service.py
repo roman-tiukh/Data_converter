66bdb58d96cf913baa6d238141862ef095e445d6
Iva-khar <ivakhar@gmail.com>
2020-03-30 23:43:33 +0300

    1

--- ratu/ratu_service.py
+++ ratu/ratu_service.py
@@ -9,91 +9,97 @@ class Ratu(Converter):
     LOCAL_FILE_NAME = config.LOCAL_FILE_NAME
     LOCAL_FOLDER = config.LOCAL_FOLDER
 
-    def save_to_db(self, data):
-        print('2')
+    tables=[
+        Region,
+        District,
+        City,
+        Citydistrict,
+        Street
+    ]
+    
+    record={
+        'RECORD': [],
+        'OBL_NAME': [],
+        'REGION_NAME': [],
+        'CITY_NAME': [],
+        'CITY_REGION_NAME': [],
+        'STREET_NAME': []
+    }
+ 
+    #creating lists for registration items that had writed to db 
+    region_list = []
+    district_list = list()
+    city_list = list()
+    citydistrict_list = list()
 
-        #clearing all db tables        
-        Region.objects.all().delete()
-        District.objects.all().delete()
-        City.objects.all().delete()
-        Citydistrict.objects.all().delete()
-        Street.objects.all().delete()
+    def save_to_db(self, record):
+               
+        print('.')
         
-        #creating lists for registration items that had writed to db 
-        region_list = []
-        district_list = list()
-        city_list = list()
-        citydistrict_list = list()
+        #writing entry to region table
+        if not record['OBL_NAME'][0] in self.region_list:
+            region = Region(
+                name=record['OBL_NAME'][0]
+                )
+            region.save()
+            self.region_list.insert(0, record['OBL_NAME'][0])
+            region=Region.objects.get(name=record['OBL_NAME'][0])
 
-        #iteration fo records in .xml file
-        for record in data['DATA']['RECORD']:
+        if record['REGION_NAME'][0]:
+            a=record['REGION_NAME'][0]
+        else:
+            a=District.EMPTY_FIELD
+        #writing entry to district table
+        if not [region, a] in self.district_list:
+            district = District(
+                region=region, 
+                name=a
+                )
+            district.save()
+            self.district_list.insert(0, [region, a])
+            district=District.objects.get(name=a, region=district.region)
 
-            print('.')
-            
-            #writing entry to region table
-            if not record['OBL_NAME'] in region_list:
-                region = Region(
-                    name=record['OBL_NAME']
-                    )
-                region.save()
-                region_list.insert(0, record['OBL_NAME'])
-                region=Region.objects.get(name=record['OBL_NAME'])
+        if record['CITY_NAME'][0]:
+            b=record['CITY_NAME'][0]
+        else:
+            b=City.EMPTY_FIELD
+        #writing entry to city table
+        if not [region, district, b] in self.city_list:
+            city = City(
+                region=region, 
+                district=district,
+                name=b
+                )
+            city.save()
+            self.city_list.insert(0, [region, district, b])
+            city=City.objects.get(name=b, region=district.region, district=city.district)
 
-            if record['REGION_NAME']:
-                a=record['REGION_NAME']
-            else:
-                a=District.EMPTY_FIELD
-            #writing entry to district table
-            if not [region, a] in district_list:
-                district = District(
-                    region=region, 
-                    name=a
-                    )
-                district.save()
-                district_list.insert(0, [region, a])
-                district=District.objects.get(name=a, region=district.region)
-
-            if record['CITY_NAME']:
-                b=record['CITY_NAME']
-            else:
-                b=City.EMPTY_FIELD
-            #writing entry to city table
-            if not [region, district, b] in city_list:
-                city = City(
-                    region=region, 
-                    district=district,
-                    name=b
-                    )
-                city.save()
-                city_list.insert(0, [region, district, b])
-                city=City.objects.get(name=b, region=district.region, district=city.district)
-
-            if record['CITY_REGION_NAME']:
-                c=record['CITY_REGION_NAME']
-            else:
-                c=Citydistrict.EMPTY_FIELD
-            
-            #writing entry to citydistrict table
-            if not [region, district, city, c] in citydistrict_list:
-                citydistrict = Citydistrict(
-                    region=region, 
-                    district=district,
-                    city=city,
-                    name=c
-                    )
-                citydistrict.save()
-                citydistrict_list.insert(0, [region, district, city, c])
-                citydistrict=Citydistrict.objects.get(name=c, region=district.region, district=city.district, city=citydistrict.city)
-            
-            #writing entry to street table
-            street = Street(
+        if record['CITY_REGION_NAME'][0]:
+            c=record['CITY_REGION_NAME'][0]
+        else:
+            c=Citydistrict.EMPTY_FIELD
+        
+        #writing entry to citydistrict table
+        if not [region, district, city, c] in self.citydistrict_list:
+            citydistrict = Citydistrict(
                 region=region, 
                 district=district,
                 city=city,
-                citydistrict=citydistrict,
-                name=record['STREET_NAME']
+                name=c
                 )
-            try:
-                street.save()
-            except:
-                None
+            citydistrict.save()
+            self.citydistrict_list.insert(0, [region, district, city, c])
+            citydistrict=Citydistrict.objects.get(name=c, region=district.region, district=city.district, city=citydistrict.city)
+        
+        #writing entry to street table
+        street = Street(
+            region=region, 
+            district=district,
+            city=city,
+            citydistrict=citydistrict,
+            name=record['STREET_NAME'][0]
+            )
+        try:
+            street.save()
+        except:
+            None
--- ratu/services.py
+++ ratu/services.py
@@ -5,6 +5,8 @@ import requests
 import sys
 import xmltodict
 import zipfile
+import xml.etree.ElementTree
+from xml.etree.ElementTree import iterparse, XMLParser, tostring
 
 #pip freeze > requirements.txt - must run after add or change import modules
 
@@ -28,13 +30,47 @@ class Converter:
     def parse_file(self):
         # encoding & parsing .xml source file
         with codecs.open(self.LOCAL_FOLDER + self.LOCAL_FILE_NAME, encoding="cp1251") as file:
-            return xmltodict.parse(file.read()) 
-                
-    def process(self): #writing .xml data to db
+            return xmltodict.parse(file.read())
+    
+    def clear_db(self):
+        # clearing data base
+        for table in self.tables:
+            table.objects.all().delete()
 
+    def process(self):
         print('4')
+        i=0
+        record=self.record
+        # get an iterable
+        context = iterparse(self.LOCAL_FOLDER + self.LOCAL_FILE_NAME, events=("start", "end"))
+        # turn it into an iterator
+        context = iter(context)
+        # get the root element
+        event, root = context.__next__()
 
-        data=self.parse_file()
-        self.save_to_db(data)
+        self.clear_db()
+
+        for event, elem in context:
+            if event == "end" and elem.tag == "RECORD":
+                i=i+1
+                print(i, '\n\n................................................................................................')
+                for text in elem.iter():
+                    print(text.tag, text.text)
+                    record[text.tag].append(text.text)
+                
+                self.save_to_db(record)
+                
+                for key in record:
+                    record[key].clear()
+                root.clear()
+
+                
+    # def process(self): #writing .xml data to db
+
+    #     print('4')
+
+    #     data=self.parse_file()
+    #     self.clear_db()
+    #     self.save_to_db(data)
         
     # -------------- end of process()
