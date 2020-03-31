63798d82edda3a796a17a7cb71b835c261798649
Iva-khar <ivakhar@gmail.com>
2020-03-31 20:17:38 +0300

    added_Fop_class

--- ratu/models/rfop_models.py
+++ ratu/models/rfop_models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 # Create your models here.
 class Rfop(models.Model):
-    fullname = models.CharField(max_length=50)
-    adress = models.CharField(max_length=100)
-    kved = models.IntegerField(default=0)
-    state = models.CharField(max_length=30)
+    fullname = models.CharField(max_length=100)
+    address = models.CharField(max_length=300)
+    kved = models.CharField(max_length=250)
+    state = models.CharField(max_length=100)
 
--- ratu/rfop_service.py
+++ ratu/rfop_service.py
@@ -1,8 +1,8 @@
 import ratu.config as config
-from ratu.models.ratu_models import Region, District, City, Citydistrict, Street
+from ratu.models.rfop_models import Rfop
 from ratu.services import Converter
 
-class Rfop(Converter):
+class Rfop_class(Converter):
     
     #paths for remote and local sours files
     FILE_URL = config.FILE_URL
@@ -22,85 +22,17 @@ class Rfop(Converter):
         'KVED': '',
         'STAN': ''
     }
- 
-    # #creating lists for registration items that had writed to db 
-    # region_list = []
-    # district_list = list()
-    # city_list = list()
-    # citydistrict_list = list()
-    
+     
     def save_to_db(self, record):
                
         #writing entry to region table
-        if not record['OBL_NAME'][0] in self.region_list:
-            global region
-            region = Region(
-                name=record['OBL_NAME'][0]
-                )
-            region.save()
-            self.region_list.insert(0, record['OBL_NAME'][0])
-            region=Region.objects.get(name=record['OBL_NAME'][0])
-
-        if record['REGION_NAME'][0]:
-            a=record['REGION_NAME'][0]
-        else:
-            a=District.EMPTY_FIELD
-        #writing entry to district table
-        if not [region, a] in self.district_list:
-            global district
-            district = District(
-                region=region, 
-                name=a
-                )
-            district.save()
-            self.district_list.insert(0, [region, a])
-            district=District.objects.get(name=a, region=district.region)
-
-        if record['CITY_NAME'][0]:
-            b=record['CITY_NAME'][0]
-        else:
-            b=City.EMPTY_FIELD
-        #writing entry to city table
-        if not [region, district, b] in self.city_list:
-            global city
-            city = City(
-                region=region, 
-                district=district,
-                name=b
-                )
-            city.save()
-            self.city_list.insert(0, [region, district, b])
-            city=City.objects.get(name=b, region=district.region, district=city.district)
-
-        if record['CITY_REGION_NAME'][0]:
-            c=record['CITY_REGION_NAME'][0]
-        else:
-            c=Citydistrict.EMPTY_FIELD
-        
-        #writing entry to citydistrict table
-        if not [region, district, city, c] in self.citydistrict_list:
-            global citydistrict
-            citydistrict = Citydistrict(
-                region=region, 
-                district=district,
-                city=city,
-                name=c
-                )
-            citydistrict.save()
-            self.citydistrict_list.insert(0, [region, district, city, c])
-            citydistrict=Citydistrict.objects.get(name=c, region=district.region, district=city.district, city=citydistrict.city)
-        
-        #writing entry to street table
-        street = Street(
-            region=region, 
-            district=district,
-            city=city,
-            citydistrict=citydistrict,
-            name=record['STREET_NAME'][0]
+        rfop = Rfop(
+            fullname=record['FIO'],
+            address=record['ADDRESS'],
+            kved=record['KVED'],
+            state=record['STAN']
             )
-        try:
-            street.save()
-        except:
-            None
+        rfop.save()
+    
         print('saved')
-    print('Ratu already imported. For start rewriting to the DB run > Ratu().process()')
+    print('Ratu already imported. For start rewriting to the DB run > Rfop_class().process()')
