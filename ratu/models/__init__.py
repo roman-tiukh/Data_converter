8aa85296d18750e94f22692285cd586c2b3d8554
Iva-khar <ivakhar@gmail.com>
2020-03-31 16:04:37 +0300

    1

--- ratu/models/__init__.py
+++ ratu/models/__init__.py
@@ -1,1 +1,2 @@
 from .ratu_models import Region, District, City, Citydistrict, Street
+from .rfop_models import Rfop
---
+++ ratu/models/rfop_models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+class Rfop(models.Model):
+    fullname = models.CharField(max_length=50)
+    adress = models.CharField(max_length=100)
+    kved = models.IntegerField(default=0)
+    state = models.CharField(max_length=30)
+
