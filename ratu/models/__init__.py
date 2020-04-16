5311807ff68e857e1a0d59088d44ad8761831fd6
IegorSoboliev <evsobolev@gmail.com>
2020-04-16 17:22:49 +0300

    Added models for kzed

--- ratu/models/__init__.py
+++ ratu/models/__init__.py
@@ -1,3 +1,4 @@
 from .ratu_models import Region, District, City, Citydistrict, Street
 from .rfop_models import Rfop
 from .ruo_models import Kved, State, Ruo, Founders
+from .kzed_models import Section, Division, Group
