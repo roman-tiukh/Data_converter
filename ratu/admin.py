7ceacaad230ed49591e0098fba8973292cb0b5b2
alex <ab.plus1986@gmail.com>
2020-04-21 14:07:29 +0300

    add spaces

--- ratu/admin.py
+++ ratu/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from ratu.models.rfop_models import Rfop
-from ratu.models.ratu_models import Region,District,City,Citydistrict,Street
+from ratu.models.ratu_models import Region, District, City, Citydistrict, Street
 
 admin.site.register(Rfop)
 admin.site.register(Region)
