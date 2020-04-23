912adc7f6fc47bc00df90be1b030a57fc283f543
Anna <ana.troyan@gmail.com>
2020-04-23 13:52:55 +0000

    Commit with changes and create dictionary fixed for pull request/koatuu_services/save koatuu to region table

--- ratu/services/koatuu_service.py
+++ ratu/services/koatuu_service.py
@@ -22,25 +22,25 @@ class KoatuuConverter(Converter):
 
     #creating a dictionary out off the values, which already exists in ratu tables
     def create_dictionary(self, table_model):
-        koatuu_dict = {} 
-        for table_record in table_model.objects.all(): # dictionary for keeping whole model class objects    
+        koatuu_dict = {}
+        for table_record in table_model.objects.all(): # dictionary for keeping whole model class objects
             if table_record.name:
                 unit_name = self.first_word(table_record.name)
                 koatuu_dict[unit_name]=table_record
-        return koatuu_dict                     
+        return koatuu_dict
 
     #storing data to all tables
     def save_to_db(self,data):
+        region_koatuu_dict = self.create_dictionary(Region)
         #getting values in json file Koatuu
-        for index, object_koatuu in enumerate(data):  
-            self.save_to_region_table(data, object_koatuu)
+        for index, object_koatuu in enumerate(data):
+            self.save_to_region_table(data, object_koatuu, region_koatuu_dict)
         print("Koatuu values saved")
 
-    #writing entry to koatuu field in region table 
-    def save_to_region_table(self, data, object_koatuu):
-        region_koatuu_dict = self.create_dictionary(Region)             
-        if (object_koatuu[self.LEVEL_ONE]!='') & (object_koatuu[self.LEVEL_TWO]==''):                
-            object_region_name = object_koatuu[self.OBJECT_NAME].upper().split()[0]            
+    #writing entry to koatuu field in region table
+    def save_to_region_table(self, data, object_koatuu, region_koatuu_dict):
+        if (object_koatuu[self.LEVEL_ONE]!='') & (object_koatuu[self.LEVEL_TWO]==''):
+            object_region_name = object_koatuu[self.OBJECT_NAME].upper().split()[0]
             if object_region_name in region_koatuu_dict:
                 region_koatuu = region_koatuu_dict[object_region_name]
                 region_koatuu.koatuu = object_koatuu[self.LEVEL_ONE]
