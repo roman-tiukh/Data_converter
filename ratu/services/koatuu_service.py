ff256aef0a00bf4c4965524ae3e9f9727d27ccbe
Anna <ana.troyan@gmail.com>
2020-04-24 09:36:12 +0000

    Last commit changes for pull request/koatuu_services/save koatuu to region table

--- ratu/services/koatuu_service.py
+++ ratu/services/koatuu_service.py
@@ -16,22 +16,22 @@ class KoatuuConverter(Converter):
     OBJECT_NAME = "Назва об'єкта українською мовою"
 
     #geting a single uppercase word from some string
-    def first_word(self, string):
-        string = string.upper().split()[0]
-        return string
+    def get_first_word(self, string):
+        return string.upper().split()[0]
 
     #creating a dictionary out off the values, which already exists in ratu tables
-    def create_dictionary(self, table_model):
+    def create_dictionary_for(self, table_model):#table_medel is one model from ratu_models
         koatuu_dict = {}
-        for table_record in table_model.objects.all(): # dictionary for keeping whole model class objects
+        table_model_objects = table_model.objects.all()
+        for table_record in table_model_objects: # dictionary for keeping whole model class objects
             if table_record.name:
-                unit_name = self.first_word(table_record.name)
+                unit_name = self.get_first_word(table_record.name)
                 koatuu_dict[unit_name]=table_record
         return koatuu_dict
 
     #storing data to all tables
     def save_to_db(self,data):
-        region_koatuu_dict = self.create_dictionary(Region)
+        region_koatuu_dict = self.create_dictionary_for(Region)
         #getting values in json file Koatuu
         for index, object_koatuu in enumerate(data):
             self.save_to_region_table(data, object_koatuu, region_koatuu_dict)
@@ -40,7 +40,7 @@ class KoatuuConverter(Converter):
     #writing entry to koatuu field in region table
     def save_to_region_table(self, data, object_koatuu, region_koatuu_dict):
         if (object_koatuu[self.LEVEL_ONE]!='') & (object_koatuu[self.LEVEL_TWO]==''):
-            object_region_name = self.first_word(object_koatuu[self.OBJECT_NAME])
+            object_region_name = self.get_first_word(object_koatuu[self.OBJECT_NAME])
             if object_region_name in region_koatuu_dict:
                 region_koatuu = region_koatuu_dict[object_region_name]
                 region_koatuu.koatuu = object_koatuu[self.LEVEL_ONE]
