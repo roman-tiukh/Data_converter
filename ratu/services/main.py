744ec47163497354d4fe33b0df7601bb2c12d1e1
Vital-jan <vitaljan@gmail.com>
2020-04-25 19:54:35 +0300

    main

--- ratu/services/main.py
+++ ratu/services/main.py
@@ -1,32 +1,129 @@
 import codecs
 from collections import defaultdict
 from django.apps import apps
+import json
 import io
+import os
 import requests
 import sys
+import time
 from xml.etree.ElementTree import iterparse, XMLParser, tostring
 import xmltodict
 import zipfile
 
 class Converter:
-
-    FILE_URL=None
-    LOCAL_FOLDER=None
-    LOCAL_FILE_NAME=None
+    
+    UPDATE_FILE_NAME = "update.cfg"
+    LOCAL_FILE_NAME = None
+    FILE_URL = None # url of remote zipfile without filename, look like as "http://hostname.ccc/lllll/mmmmm/"
+    LOCAL_FOLDER = "unzipped_xml/" # local folder for unzipped xml files
+    DOWNLOAD_FOLDER = "download/" # folder to downloaded files
+    DOWNLOADED_FILE_NAME = None # destination local filename
+    URLS_DICT = {}
     
     def __init__(self):
         return 
 
-    def unzip_file(self):
-        # getting zip file  from FILE_URL & extracting to LOCAL_FOLDER
+    def rename_files (self):
+        # abstract method for rename unzipped files for each app
+        return ""
+
+    def is_update (self, current_size):
+        # returns true, if file size at the <url> changed compared to current_size 
+        
         try:
-            r = requests.get(self.FILE_URL)
-        except TimeoutError as err:   
-            print ("Error open zip file " + self.FILE_URL)
-            return 
-        zip_file = zipfile.ZipFile(io.BytesIO(r.content))
-        zip_file.extractall(self.LOCAL_FOLDER)
+            with open(self.UPDATE_FILE_NAME) as file:
+                try:
+                    self.URLS_DICT = json.load(file)
+                except:
+                    pass
+        except: 
+            pass
+
+        if len (self.URLS_DICT) > 0:
+            if self.FILE_URL in self.URLS_DICT:
+                if int(self.URLS_DICT[self.FILE_URL]) == current_size:
+                    return False
+
+        return True
+
+    def change_update (self, current_size):
+        # update json, contains url & remote files size 
+
+        self.URLS_DICT[self.FILE_URL] = current_size
+        file = open (self.UPDATE_FILE_NAME, "w")
+        file.write (json.dumps(self.URLS_DICT))
+        file.close
+
+    def download_file(self):
+        # getting remote file from self.file_url
+        # returns 0 if operation is succefully or another value if error occured
+
+        #request to remote url
+        print ("Request to remote url > " + self.FILE_URL)
+        response = requests.get(self.FILE_URL, stream=True) 
+        print ("Response: " + str(response.status_code))
+        if (response.status_code != 200):
+            print ("ERROR of requests.get(" + self.file_url + ") in module ratu/main.py")
+            return 1
+
+        # check for remote file updates 
+        file_size = int (response.headers['Content-Length'])
+        if not ( self.is_update (file_size) ):
+            print ("Source files are not updated. Nothing to download.")
+            return 1
+
+        # download file
+        with open(self.DOWNLOAD_FOLDER + self.DOWNLOADED_FILE_NAME, 'wb') as fd:
+            print ("Download zip file: " + fd.name + " (" + str(file_size) + " bytes total) ...")
+            done = 0
+            buffer_size = 102400
+            step = 10
+
+            for chunk in response.iter_content(chunk_size=buffer_size):
+                fd.write(chunk)
+                done += buffer_size
+                percent = round(( done / file_size * 100 ))
+                if (percent >= step):
+                    if percent > 100: percent = 100
+                    print ( str ( percent ) + "%")
+                    step += 10
+
+            if (os.stat(self.DOWNLOAD_FOLDER + self.DOWNLOADED_FILE_NAME).st_size == file_size):
+                print ("File downloaded succefully.")
+                self.change_update (file_size)
+                return 0
+            else: 
+                print ("Download file error")
+                return 2
+
+    def unzip_file (self):
+        # unzip downloaded file
+        print("Unzipping file ...") 
+        try:
+            zip_file = zipfile.ZipFile(self.DOWNLOAD_FOLDER + self.DOWNLOADED_FILE_NAME)
+            zip_file.extractall(self.LOCAL_FOLDER)
+        except:
+            print ("ERROR unzip file")
+            return 1
+
+        # remove zip file
+        try:
+            os.remove (self.DOWNLOAD_FOLDER + self.DOWNLOADED_FILE_NAME) 
+        except:
+            print('Deleting zipfile error!')
             
+        print ("Unzip succefully.")
+        return 0
+
+    def rename_files (self):
+        # renames unzipped files to short statical names
+        files = os.listdir (self.LOCAL_FOLDER)
+
+        for file in files:
+            new_filename = self.rename(file)
+            if (new_filename != ""): os.rename(self.LOCAL_FOLDER + file, self.LOCAL_FOLDER + new_filename)
+
     def parse_file(self):
         # encoding & parsing .xml source file
         with codecs.open(self.LOCAL_FOLDER + self.LOCAL_FILE_NAME, encoding="cp1251") as file:
