cc4a3f8a4b05cc002c952462a90f4cc0292bfae0
alex <ab.plus1986@gmail.com>
2020-04-30 10:25:13 +0300

    change max_length

--- ratu/serializers/ratu_serializers.py
+++ ratu/serializers/ratu_serializers.py
@@ -1,29 +1,29 @@
 from rest_framework import serializers
 
 class RegionSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    koatuu = serializers.CharField()
+    name = serializers.CharField(max_length=30)
+    koatuu = serializers.CharField(max_length=10)
     
 class DistrictSerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=100)
+    region = serializers.CharField(max_length=30)
     name = serializers.CharField(max_length=100)
     koatuu = serializers.CharField()
     
 class CitySerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=100)
+    region = serializers.CharField(max_length=30)
     district = serializers.CharField(max_length=100)
     name = serializers.CharField(max_length=100)
     koatuu = serializers.CharField()
 
 class CitydistrictSerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=100)
+    region = serializers.CharField(max_length=30)
     district = serializers.CharField(max_length=100)
     city = serializers.CharField(max_length=100)
     name = serializers.CharField(max_length=100)
     koatuu = serializers.CharField()
     
 class StreetSerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=100)
+    region = serializers.CharField(max_length=30)
     district = serializers.CharField(max_length=100)
     city = serializers.CharField(max_length=100)
     citydistrict = serializers.CharField(max_length=100)
