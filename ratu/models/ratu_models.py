0e05b29b7ebbbba6b9405f68b04dcf15a0965e99
IegorSoboliev <evsobolev@gmail.com>
2020-04-30 14:45:16 +0300

    Added Category in ratu_models

--- ratu/models/ratu_models.py
+++ ratu/models/ratu_models.py
@@ -3,14 +3,21 @@ from django.db import models
 class Region(models.Model):
     name = models.CharField(max_length=30, unique=True)
     koatuu = models.CharField(max_length=10, unique=True, null=True)
+
     def __str__(self):
         return self.name
 
 class District(models.Model):
-    EMPTY_FIELD = 'empty field'
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     koatuu = models.CharField(max_length=10, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = (models.CharField(max_length=5, unique=True, null=True))
+
     def __str__(self):
         return self.name
 
@@ -18,8 +25,10 @@ class City(models.Model):
     EMPTY_FIELD = 'empty field'
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     koatuu = models.CharField(max_length=10, unique=True, null=True)
+    
     def __str__(self):
         return self.name
 
@@ -28,8 +37,10 @@ class Citydistrict(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     koatuu = models.CharField(max_length=10, unique=True, null=True)
+
     def __str__(self):
         return self.name
 
@@ -39,5 +50,6 @@ class Street(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     citydistrict = models.ForeignKey(Citydistrict, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
