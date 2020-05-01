74c141737eac5bc5db9f5635c6f419eb75f5a777
Vital-jan <vitaljan@gmail.com>
2020-05-01 22:15:37 +0300

    script

--- script.py
+++ script.py
@@ -1,14 +1,16 @@
-from data_ocean.services.rfop_service import RfopConverter
 from data_ocean.services.ruo_service import RuoConverter
 from data_ocean.services.ratu_service import RatuConverter
+from data_ocean.services.rfop_service import RfopConverter
 
 if RatuConverter().download_file() == 0:
     if RatuConverter().unzip_file() == 0:
         RatuConverter().rename_files()
-        # RatuConverter().process()
+
+# RatuConverter().process()
 
 if RfopConverter().download_file() == 0:
     if RfopConverter().unzip_file() == 0:
         RfopConverter().rename_files()
-        # RfopConverter().process()
-        # RuoConverter().process()
+
+# RfopConverter().process()
+RuoConverter().process()
