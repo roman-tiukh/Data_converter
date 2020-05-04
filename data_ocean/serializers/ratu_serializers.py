c00eb7b6d37a1bc56e762507da35171994e3bfdc
alex <ab.plus1986@gmail.com>
2020-05-04 22:57:27 +0300

    edit3_ ratu_serializers

--- data_ocean/serializers/ratu_serializers.py
+++ data_ocean/serializers/ratu_serializers.py
@@ -1,28 +1,37 @@
 from rest_framework import serializers
 from data_ocean.models.ratu_models import Region, District, City, Citydistrict, Street
 
-
 class RegionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Region
-        fields = ('id', 'name', 'koatuu')
-   
+        fields = '__all__'
+
 class DistrictSerializer(serializers.ModelSerializer):
+    region = serializers.CharField(max_length=30)
     class Meta:
         model = District
-        fields = ('id', 'region', 'name', 'koatuu')
-    
+        fields = '__all__'
+        
 class CitySerializer(serializers.ModelSerializer):
+    region = serializers.CharField(max_length=30)
+    district = serializers.CharField(max_length=100)
     class Meta:
         model = City
-        fields = ('id', 'region', 'district', 'name', 'koatuu')
+        fields = '__all__'
     
 class CitydistrictSerializer(serializers.ModelSerializer):
+    region = serializers.CharField(max_length=30)
+    district = serializers.CharField(max_length=100)
+    city = serializers.CharField(max_length=100)
     class Meta:
         model = Citydistrict
-        fields = ('id', 'region', 'district', 'city', 'name', 'koatuu')
+        fields = '__all__'
     
 class StreetSerializer(serializers.ModelSerializer):
+    region = serializers.CharField(max_length=30)
+    district = serializers.CharField(max_length=100)
+    city = serializers.CharField(max_length=100)
+    citydistrict = serializers.CharField(max_length=100)
     class Meta:
         model = Street
-        fields = ('id', 'region', 'district', 'city', 'citydistrict', 'name')
+        fields = '__all__'
