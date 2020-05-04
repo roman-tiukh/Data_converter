c1660d6e6a9f85a6eb794ac196faf1871dc63892
alex <ab.plus1986@gmail.com>
2020-05-04 16:02:11 +0300

    edit ratu_serializers

--- data_ocean/serializers/ratu_serializers.py
+++ data_ocean/serializers/ratu_serializers.py
@@ -1,30 +1,29 @@
 from rest_framework import serializers
+from data_ocean.models.ratu_models import Region, District, City, Citydistrict, Street
 
-class RegionSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=30)
-    koatuu = serializers.CharField(max_length=10)
-    
-class DistrictSerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=30)
-    name = serializers.CharField(max_length=100)
-    koatuu = serializers.CharField(max_length=10)
-    
-class CitySerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=30)
-    district = serializers.CharField(max_length=100)
-    name = serializers.CharField(max_length=100)
-    koatuu = serializers.CharField(max_length=10)
 
-class CitydistrictSerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=30)
-    district = serializers.CharField(max_length=100)
-    city = serializers.CharField(max_length=100)
-    name = serializers.CharField(max_length=100)
-    koatuu = serializers.CharField(max_length=10)
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ('id', 'name', 'koatuu')
+   
+class DistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = District
+        fields = ('id', 'region', 'name', 'koatuu')
     
-class StreetSerializer(serializers.Serializer):
-    region = serializers.CharField(max_length=30)
-    district = serializers.CharField(max_length=100)
-    city = serializers.CharField(max_length=100)
-    citydistrict = serializers.CharField(max_length=100)
-    name = serializers.CharField(max_length=100)
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ('id', 'region', 'district', 'name', 'koatuu')
+    
+class CitydistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Citydistrict
+        fields = ('id', 'region', 'district', 'city', 'name', 'koatuu')
+    serializers.CharField(max_length=10)
+    
+class StreetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Street
+        fields = ('id', 'region', 'district', 'city', 'citydistrict', 'name')
