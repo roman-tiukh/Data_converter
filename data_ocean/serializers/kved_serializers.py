55ab9de0135815bf874dfa972ce46c1a4c8d64e6
IegorSoboliev <evsobolev@gmail.com>
2020-05-05 11:13:12 +0300

    Added Kved serializer with group, division, section names

--- data_ocean/serializers/kved_serializers.py
+++ data_ocean/serializers/kved_serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from data_ocean.models.kved_models import Kved
+from data_ocean.models.kved_models import Kved, Group, Division, Section
 
 class KvedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Kved
         fields = ['code', 'name']
+
+class FullKvedSerializer(serializers.ModelSerializer):
+    group = serializers.CharField(max_length=500)
+    division = serializers.CharField(max_length=500)    
+    section = serializers.CharField(max_length=500)
+    
+    class Meta:
+        model = Kved
+        fields = ['code', 'name', 'group', 'division', 'section']
