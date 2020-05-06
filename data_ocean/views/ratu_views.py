b5ffbacc2ace76d6413032f87e47ec6b67607f0f
alex <ab.plus1986@gmail.com>
2020-05-06 13:52:15 +0300

    change ratu_views_&_urls

--- data_ocean/views/ratu_views.py
+++ data_ocean/views/ratu_views.py
@@ -1,36 +1,87 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 from data_ocean.models.ratu_models import Region, District, City, Citydistrict, Street
 from data_ocean.serializers.ratu_serializers import RegionSerializer, CitySerializer, CitydistrictSerializer, StreetSerializer, DistrictSerializer
-from data_ocean.views.views import Views
-from data_converter.pagination import CustomPagination
 
-class RegionView(Views):
-    serializer_class = RegionSerializer
+class RegionView(viewsets.ReadOnlyModelViewSet, PageNumberPagination):
     queryset = Region.objects.all()
-    serializer = RegionSerializer(queryset, many=True)
-    pagination_class = CustomPagination
+    serializer_class = RegionSerializer
 
-class DistrictView(Views):
-    serializer_class = DistrictSerializer
+    def list(self, request):
+        queryset = self.get_queryset()
+        results = self.paginate_queryset(queryset)
+        serializer = RegionSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        region = get_object_or_404(queryset, pk=pk)
+        serializer = RegionSerializer(region)
+        return Response(serializer.data) 
+
+class DistrictView(viewsets.ReadOnlyModelViewSet, PageNumberPagination):
     queryset = District.objects.all()
-    serializer = DistrictSerializer(queryset, many=True)
-    pagination_class = CustomPagination
+    serializer_class = DistrictSerializer
 
-class CityView(Views):
-    serializer_class = CitySerializer
+    def list(self, request):
+        queryset = self.get_queryset()
+        results = self.paginate_queryset(queryset)
+        serializer = DistrictSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        district = get_object_or_404(queryset, pk=pk)
+        serializer = DistrictSerializer(district)
+        return Response(serializer.data) 
+
+class CityView(viewsets.ReadOnlyModelViewSet, PageNumberPagination):
     queryset = City.objects.all()
-    serializer = CitySerializer(queryset, many=True)
-    pagination_class = CustomPagination
+    serializer_class = CitySerializer
 
-class CitydistrictView(Views):
-    serializer_class = CitydistrictSerializer
+    def list(self, request):
+        queryset = self.get_queryset()
+        results = self.paginate_queryset(queryset)
+        serializer = CitySerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        city = get_object_or_404(queryset, pk=pk)
+        serializer = CitySerializer(city)
+        return Response(serializer.data) 
+   
+class CitydistrictView(viewsets.ReadOnlyModelViewSet, PageNumberPagination):
     queryset = Citydistrict.objects.all()
-    serializer = CitydistrictSerializer(queryset, many=True)
-    pagination_class = CustomPagination
+    serializer_class = CitydistrictSerializer
 
-class StreetView(Views):
-    serializer_class = StreetSerializer
+    def list(self, request):
+        queryset = self.get_queryset()
+        results = self.paginate_queryset(queryset)
+        serializer = CitydistrictSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        citydistrict = get_object_or_404(queryset, pk=pk)
+        serializer = CitydistrictSerializer(citydistrict)
+        return Response(serializer.data)
+    
+class StreetView(viewsets.ReadOnlyModelViewSet, PageNumberPagination):
     queryset = Street.objects.all()
-    serializer = StreetSerializer(queryset, many=True)
-    pagination_class = CustomPagination
+    serializer_class = StreetSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        results = self.paginate_queryset(queryset)
+        serializer = StreetSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        street = get_object_or_404(queryset, pk=pk)
+        serializer = StreetSerializer(street)
+        return Response(serializer.data)
