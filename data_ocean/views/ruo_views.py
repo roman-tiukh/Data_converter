05f0839a1046522ccf9bd16ec0d65bec64020468
Iva-khar <ivakhar@gmail.com>
2020-05-07 13:23:03 +0300

    ruo_views

--- data_ocean/views/ruo_views.py
+++ data_ocean/views/ruo_views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+
 from data_ocean.models.ruo_models import Ruo
 from data_ocean.serializers.ruo_serializers import RuoSerializer
-from data_converter.pagination import CustomPagination
-from data_ocean.views.views import Views
 
-class RuoView(Views):
-    serializer_class = RuoSerializer
+class RuoView(viewsets.ReadOnlyModelViewSet, PageNumberPagination):
     queryset = Ruo.objects.all()
-    serializer = RuoSerializer(queryset, many=True)
-    pagination_class = CustomPagination
+    serializer_class = RuoSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        results = self.paginate_queryset(queryset)
+        serializer = RuoSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        ruo = get_object_or_404(queryset, pk=pk)
+        serializer = RuoSerializer(ruo)
+        return Response(serializer.data)
