2da8d6bc11fb9e77fa54d0549ff29018809d61a4
IegorSoboliev <evsobolev@gmail.com>
2020-05-08 13:00:55 +0300

    Common models for Status, Authority and Taxpayer

--- data_ocean/models/__init__.py
+++ data_ocean/models/__init__.py
@@ -2,3 +2,4 @@ from .ratu_models import Region, District, City, Citydistrict, Street
 from .rfop_models import Rfop
 from .ruo_models import State, Ruo, Founders
 from .kved_models import Section, Division, Group, Kved
+from .common_models import Status, Authority, TaxpayerType
