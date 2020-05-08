5e5b1581b7ff8ea6def75c65ab7ef2d635fa32c3
Iva-khar <ivakhar@gmail.com>
2020-05-08 20:23:51 +0300

    migration

--- data_ocean/models/__init__.py
+++ data_ocean/models/__init__.py
@@ -3,3 +3,6 @@ from .rfop_models import Rfop, Fop, FopToKved, ExchangeData
 from .ruo_models import State, Ruo, Founders
 from .kved_models import Section, Division, Group, Kved
 from .common_models import Status, Authority, TaxpayerType
+from .company_models import (Assignee, BancruptcyReadjustment, Bylaw, Company, 
+                            CompanyDetail, CompanyToKved, CompanyToPredecessor, CompanyType, 
+                            ExchangeDataCompany, FounderFull, Predecessor, Signer, TerminationStarted)
