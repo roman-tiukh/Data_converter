b57e758f033043a8fff3632a3b312413b045bd74
Roman Tiukh <frogour@gmail.com>
2020-06-11 20:47:58 +0300

    fix auth

--- users/models.py
+++ users/models.py
@@ -9,15 +9,11 @@ class DataOceanUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, last_name, first_name, email, password, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
 
-        if not last_name:
-            raise ValueError(_('The Last Name must be set'))
-        if not first_name:
-            raise ValueError(_('The First Name must be set'))
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
@@ -26,7 +22,7 @@ class DataOceanUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, last_name, first_name, email, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -37,7 +33,7 @@ class DataOceanUserManager(BaseUserManager):
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(last_name, first_name, email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 
 class DataOceanUser(AbstractUser):
