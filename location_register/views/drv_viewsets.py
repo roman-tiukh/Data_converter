b2ca97107e974fc898814bb88aaf44238e027f58
alex <ab.plus1986@gmail.com>
2020-06-11 14:34:22 +0300

    add cache to loc_register

--- location_register/views/drv_viewsets.py
+++ location_register/views/drv_viewsets.py
@@ -16,14 +16,4 @@ class DrvBuildingViewSet(CachedViewMixin,viewsets.ReadOnlyModelViewSet):
     queryset = DrvBuilding.objects.all()
     serializer_class = DrvBuildingSerializer
 
-    # def list(self, request):
-    #     queryset = self.get_queryset()
-    #     results = self.paginate_queryset(queryset)
-    #     serializer = DrvBuildingSerializer(results, many=True)
-    #     return self.get_paginated_response(serializer.data)
-
-    # def retrieve(self, request, pk=None):
-    #     queryset = self.get_queryset()
-    #     region = get_object_or_404(queryset, pk=pk)
-    #     serializer = DrvBuildingSerializer(region)
-    #     return Response(serializer.data)
+    
