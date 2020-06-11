df1d28633dcfe08bce6b35a7bc427217d1176fe0
PyDev777 <pydev@ukr.net>
2020-06-11 16:35:39 +0300

    Update data_converter/settings.py
    Co-authored-by: Roman Tiukh <38906004+roman-tiukh@users.noreply.github.com>

--- data_converter/settings.py
+++ data_converter/settings.py
@@ -77,6 +77,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     'https://dataocean.us',
     'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
