8b5cc737dd8f0357bce6a328d808b76742784c48
alex <ab.plus1986@gmail.com>
2020-06-11 11:57:19 +0300

    add cache to location_register

--- location_register/views/drv_viewsets.py
+++ location_register/views/drv_viewsets.py
@@ -1,24 +1,29 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, viewsets
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
-
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from location_register.models.drv_models import DrvBuilding
 from location_register.serializers.drv_serializers import DrvBuildingSerializer
+from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
+from .ratu_viewsets import CachedViewMixin
 
 
+<<<<<<< HEAD
 class DrvBuildingViewSet(viewsets.ReadOnlyModelViewSet):
+=======
+class DrvBuildingViewSet(CachedViewMixin,viewsets.ReadOnlyModelViewSet):
+    pagination_class = PageNumberPagination
+>>>>>>> add cache to location_register
     queryset = DrvBuilding.objects.all()
     serializer_class = DrvBuildingSerializer
 
-    def list(self, request):
-        queryset = self.get_queryset()
-        results = self.paginate_queryset(queryset)
-        serializer = DrvBuildingSerializer(results, many=True)
-        return self.get_paginated_response(serializer.data)
+    # def list(self, request):
+    #     queryset = self.get_queryset()
+    #     results = self.paginate_queryset(queryset)
+    #     serializer = DrvBuildingSerializer(results, many=True)
+    #     return self.get_paginated_response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = self.get_queryset()
-        region = get_object_or_404(queryset, pk=pk)
-        serializer = DrvBuildingSerializer(region)
-        return Response(serializer.data)
+    # def retrieve(self, request, pk=None):
+    #     queryset = self.get_queryset()
+    #     region = get_object_or_404(queryset, pk=pk)
+    #     serializer = DrvBuildingSerializer(region)
+    #     return Response(serializer.data)
--- location_register/views/ratu_viewsets.py
+++ location_register/views/ratu_viewsets.py
@@ -1,11 +1,19 @@
+<<<<<<< HEAD
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
+=======
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+>>>>>>> add cache to location_register
 from location_register.models.ratu_models import Region, District, City, Citydistrict, Street
 from location_register.serializers.ratu_serializers import RegionSerializer, DistrictSerializer, CitySerializer, CitydistrictSerializer, \
     StreetSerializer
+from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
 
 
+<<<<<<< HEAD
 class RegionView(viewsets.ReadOnlyModelViewSet):
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
@@ -89,3 +97,35 @@ class StreetView(viewsets.ReadOnlyModelViewSet):
         street = get_object_or_404(queryset, pk=pk)
         serializer = StreetSerializer(street)
         return Response(serializer.data)
+=======
+class CachedViewMixin:
+    @method_decorator(cache_page(60*15))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+class RegionView(CachedViewMixin,viewsets.ReadOnlyModelViewSet):
+    pagination_class = PageNumberPagination
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+    
+class DistrictView(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    pagination_class = PageNumberPagination
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+
+class CityView(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    pagination_class = PageNumberPagination
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+class CitydistrictView(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    pagination_class = PageNumberPagination
+    queryset = Citydistrict.objects.all()
+    serializer_class = CitydistrictSerializer
+    
+class StreetView(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    pagination_class = PageNumberPagination
+    queryset = Street.objects.all()
+    serializer_class = StreetSerializer
+
+>>>>>>> add cache to location_register
