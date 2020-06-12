8d70d1471ef6fb17309045098c65889a2d2bf058
Anna <ana.troyan@gmail.com>
2020-06-12 17:02:46 +0300

    Deleting hard code from tasks

--- location_register/tasks.py
+++ location_register/tasks.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 import json
+from django.conf import settings
 from celery import shared_task
 from location_register.converter.ratu import RatuConverter
 from location_register.converter.koatuu import KoatuuConverter
 
-
-LOCAL_FILE = "/home/solomia/Documentos/Project/docean/src/Data_converter/source_data/koatuu.json"
+LOCAL_FILE_NAME = settings.LOCATION_KOATUU_LOCAL_FILE_NAME
 
 @shared_task
 def fill_in_ratu_table():
@@ -14,6 +14,6 @@ def fill_in_ratu_table():
 
 @shared_task
 def fill_in_koatuu_table():
-    with open(LOCAL_FILE) as json_file:
+    with open(LOCAL_FILE_NAME) as json_file:
         data = json.load(json_file)
     KoatuuConverter().save_to_db(data)
