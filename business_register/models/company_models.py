5d8e90d970746fc28d341f40c30d9ecade5295e5
Iegor Soboliev <58389602+IegorSoboliev@users.noreply.github.com>
2020-06-16 12:45:29 +0300

    Fixed FounderNew related_name (#156)

--- business_register/models/company_models.py
+++ business_register/models/company_models.py
@@ -84,15 +84,8 @@ class FounderFull(DataOceanModel):
     equity = models.FloatField(null=True)
     hash_code = models.CharField(max_length=510)
 
-class FounderFull(DataOceanModel):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='founders')
-    name = models.TextField(null=True)
-    edrpou = models.CharField(max_length=9, null=True)
-    equity = models.FloatField(null=True)
-    hash_code = models.CharField(max_length=510)    
-
 class FounderNew(DataOceanModel):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='founders')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='founders_new')
     name = models.TextField(null=True)
     edrpou = models.CharField(max_length=9, null=True)
     equity = models.FloatField(null=True)
