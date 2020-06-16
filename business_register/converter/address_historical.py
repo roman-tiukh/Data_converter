db67fbad066942fa0d465f603561d7b873760beb
Iva-khar <ivakhar@gmail.com>
2020-06-16 18:38:29 +0300

    fix_after_PR

--- business_register/converter/address_historical.py
+++ business_register/converter/address_historical.py
@@ -2,13 +2,13 @@ import datetime
 from django.apps import apps
 
 from business_register.models.company_models import Company
-from data_converter import settings_local
+from django.conf import settings
 from data_ocean.converter import Converter
 
 
-class Historical(Converter):
-    LOCAL_FILE_NAME = settings_local.LOCAL_FILE_NAME_UO_ADDRESS
-    LOCAL_FOLDER = settings_local.LOCAL_FOLDER
+class AddressHistorical(Converter):
+    LOCAL_FILE_NAME = settings.LOCAL_FILE_NAME_UO_ADDRESS
+    LOCAL_FOLDER = settings.LOCAL_FOLDER
     CHUNK_SIZE = 500
     RECORD_TAG = 'DATA_RECORD'
     HistoricalCompany = apps.get_model('business_register', 'HistoricalCompany')
@@ -24,9 +24,10 @@ class Historical(Converter):
                 record.xpath('DATE')[0].text,
                 "%Y/%m/%d %H:%M:%S"
             ).strftime("%Y-%m-%d %H:%M:%S")
-            if Company.objects.filter(edrpou=company.edrpou).first():
-                company.id = Company.objects.filter(edrpou=company.edrpou).first().id
-            else:
+            try:
+                company_exists = Company.objects.filter(edrpou=company.edrpou).first()
+                company.id = company_exists.id
+            except AttributeError:
                 company.id = 0 #for changed records that can't be assigned to existing company
             company.history_type = '~'
             company.hash_code = record.xpath('NAME')[0].text + record.xpath('EDRPOU')[0].text
