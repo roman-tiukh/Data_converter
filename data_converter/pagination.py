5a64e1124632548203a6cfc09d818a4d51a9e2b7
alex <ab.plus1986@gmail.com>
2020-06-24 10:03:05 +0300

    add CustomPagination2

--- data_converter/pagination.py
+++ data_converter/pagination.py
@@ -2,11 +2,8 @@ import math
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-DEFAULT_PAGE = 1
-
 
 class CustomPagination(PageNumberPagination):
-    page = DEFAULT_PAGE
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
