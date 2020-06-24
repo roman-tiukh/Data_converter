3cf3fd23bda69ea0ba0c2095f0d67cba3346043b
Iva-khar <ivakhar@gmail.com>
2020-06-24 12:30:54 +0300

    historical branch

--- business_register/converter/history.py
+++ business_register/converter/history.py
@@ -212,3 +212,41 @@ class CapitalHistorical(Converter):
             self.create_queues.append(company_detail)
         self.HistoricalCompanyDetail.objects.bulk_create(self.create_queues)
         self.create_queues = []
+
+
+class BranchHistorical(Converter):
+    LOCAL_FILE_NAME = settings.LOCAL_FILE_NAME_UO_BRANCH
+    LOCAL_FOLDER = settings.LOCAL_FOLDER
+    CHUNK_SIZE = 2000
+    RECORD_TAG = 'DATA_RECORD'
+    HistoricalCompany = apps.get_model('business_register', 'HistoricalCompany')
+    tables = [HistoricalCompany]
+    create_queues = []
+
+    def save_to_db(self, records):
+        for record in records:
+            branch = self.HistoricalCompany()
+            # branch.edrpou = record.xpath('')[0].text
+            branch.name = record.xpath('BRANCH_NAME')[0].text
+            if len (record.xpath('BRANCH_CODE')) > 0:
+                branch.short_name = record.xpath('BRANCH_CODE')[0].text
+            else:
+                branch.short_name = ''
+            branch.history_date = datetime.datetime.strptime(
+                record.xpath('DATE')[0].text,
+                "%Y/%m/%d %H:%M:%S"
+            ).strftime("%Y-%m-%d %H:%M:%S")
+            try:
+                company_exists = Company.objects.filter(
+                    edrpou=record.xpath('EDRPOU')[0].text).first()
+                branch.id = company_exists.id
+                branch.parent = company_exists
+            except AttributeError:
+                branch.id = 0 #for changed records that can't be assigned to existing company
+            branch.history_type = HistoryTypes.UPDATE
+            branch.hash_code = record.xpath('BRANCH_NAME')[0].text +\
+                branch.short_name
+            branch.created_at = datetime.datetime.now()
+            self.create_queues.append(branch)
+        self.HistoricalCompany.objects.bulk_create(self.create_queues)
+        self.create_queues = []
