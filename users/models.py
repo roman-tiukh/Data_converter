4a24bff1d56ef258a4ff59dc8193dcaac528da55
dev <pydev777@gmail.com>
2020-06-26 13:02:42 +0300

    Remove unused import

--- users/models.py
+++ users/models.py
@@ -1,11 +1,7 @@
-from datetime import datetime, timedelta
-from random import randint
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 
 
 class DataOceanUserManager(BaseUserManager):
