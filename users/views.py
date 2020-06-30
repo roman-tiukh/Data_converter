fe76ea3d335eb836d14d627f122073957949301a
dev <pydev777@gmail.com>
2020-06-30 07:36:23 +0300

    Comments in English

--- users/views.py
+++ users/views.py
@@ -34,12 +34,11 @@ class CustomRegisterView(views.APIView):
 
     def post(self, request: Request, *args, **kwargs):
 
-        # отримати і перевірити дані
         serializer = CustomRegisterSerializer(data=request.data)
         if not serializer.is_valid():
             return Response(serializer.errors, status=400)
 
-        # створити змінну кандидата, заповнити провалідованими даними
+        # create a candidate variable, fill in the validated data
         user = CandidateUserModel(
             email=serializer.validated_data.get('email'),
             password=make_password(serializer.validated_data.get('password1')),  # hash
@@ -47,39 +46,39 @@ class CustomRegisterView(views.APIView):
             last_name=serializer.validated_data.get('last_name'),
         )
 
-        # перевірити, чи є цей email серед існуючих користувачів
+        # check if this email is among existing users
         if DataOceanUser.objects.filter(email=user.email).exists():
             return Response({'detail': _('User with this email is already exists')}, status=400)
 
-        # видалити кандидата з CandidateUserModel, якщо існує (щоб не використовувався старий ID)
+        # remove the candidate from CandidateUserModel, if exists (old ID is not used)
         CandidateUserModel.objects.filter(email=user.email).delete()
 
-        # створити мітку часу закінчення дії реєстрації з цим кодом підтвердження
+        # create a stamp of registration end time for this confirmation code
         user.expire_at = timezone.now() + timedelta(minutes=settings.CANDIDATE_EXPIRE_MINUTES)
 
-        # створити код підтвердження за HMAC
+        # generate a HMAC verification code
         msg = user.expire_at.strftime("%Y%m%dT%H%M%S%fZ") + user.email
         user.confirm_code = hmac.new(
             key=settings.SECRET_KEY.encode(),
             msg=msg.encode('utf-8'),
-            digestmod=hashlib.sha256  # md5
+            digestmod=hashlib.sha256
         ).hexdigest()
 
-        # додати кандидата в CandidateUserModel (з новим ID)
+        # add candidate to CandidateUserModel (with new ID)
         user.save()
 
-        # створити лист кандидату для підтвердження email
+        # create a letter to the candidate to confirm the email
         confirm_link = f'{settings.FRONTEND_SITE_URL}/api/custom/registration-confirm/{user.id}/{user.confirm_code}'
         subject = 'Підтвердження реєстрації на Data Ocean'
         text = f"{user.first_name}, ми отримали запит на реєстрацію у системі Data Ocean. Щоб підтвердити адресу Вашої електронної пошти, перейдіть за цим посиланням {confirm_link} \r\nЯкщо Вами ці дії не проводились, проігноруйте цей лист. \r\nЗ повагою, Data Ocean"
-        # відправити лист
+        # send mail
         if settings.SEND_MAIL_BY_POSTMAN:
-            # з використанням POSTMAN
+            # use POSTMAN
             send_status_code = send_plain_mail(user.email, subject, text)
             if send_status_code != 201:
                 return Response({'detail': _('Email not sent. Try again later.')}, status=400)
         else:
-            # з використанням EMAIL_BACKEND
+            # use EMAIL_BACKEND
             send_mail(subject, text, settings.DEFAULT_FROM_EMAIL, [user.email, ], fail_silently=True)
 
         return Response({"email": user.email, "first_name": user.first_name, "last_name": user.last_name}, status=200)
@@ -90,28 +89,28 @@ class CustomRegisterConfirmView(views.APIView):
 
     def post(self, request: Request, user_id: int, confirm_code: str, *args, **kwargs):
 
-        # знайти user_id в таблиці кандидатів
+        # find user_id in the candidate table
         user = CandidateUserModel.objects.filter(id=user_id).first()
         if not user:
             return Response({'detail': _('Confirmation link is broken')}, status=400)
 
-        # перевірити, чи не закіінчився строк дії кода підтвердження
+        # check that the confirmation code has expired
         if timezone.now() > user.expire_at:
             return Response({'detail': _('Confirmation link is expired.')}, status=400)
 
-        # створити код підтвердження за HMAC і порівняти із вхідним
+        # create an HMAC verification code and compare with the incoming one
         msg = user.expire_at.strftime("%Y%m%dT%H%M%S%fZ") + user.email
         confirm_code_check = hmac.new(
             key=settings.SECRET_KEY.encode(),
             msg=msg.encode('utf-8'),
-            digestmod=hashlib.sha256  # md5
+            digestmod=hashlib.sha256
         ).hexdigest()
 
-        # якщо код не співпадає
+        # if the code does not match
         if confirm_code != confirm_code_check:
             return Response({'detail': _('Confirmation link is broken')}, status=400)
 
-        # створити реального користувача
+        # create a real user
         real_user = DataOceanUser(
             email=user.email,
             password=user.password,
@@ -120,17 +119,17 @@ class CustomRegisterConfirmView(views.APIView):
         )
         real_user.save()
 
-        # створити лист користувачу з привітанням
+        # create a greeting to the user
         subject = 'Ласкаво просимо у Data Ocean!'
         text = f"Дякуємо за реєстрацію у системі Data Ocean. \r\nData Ocean — провайдер зручного доступу до відкритих даних. Ми розробляємо спеціальний сервіс, де Ви зможете отримати доступ до всіх важливих та існуючих регістрів, даних та звітностей. \r\n \r\nЯкщо у Вас з'явились питання або пропозиції, зв'яжіться з нами info@dataocean.us. Будемо раді співпрацювати з Вами. Запевняємо, що наша співпраця буде плідною! \r\nЗ повагою, Data Ocean"
-        # відправити лист
+        # send mail
         if settings.SEND_MAIL_BY_POSTMAN:
-            # з використанням POSTMAN
+            # use POSTMAN
             send_status_code = send_plain_mail(user.email, subject, text)
             if send_status_code != 201:
                 return Response({'detail': _('Email not sent. Try again later.')}, status=400)
         else:
-            # з використанням EMAIL_BACKEND
+            # use EMAIL_BACKEND
             send_mail(subject, text, settings.DEFAULT_FROM_EMAIL, [user.email, ], fail_silently=True)
 
         return Response({
