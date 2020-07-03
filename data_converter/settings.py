d247097039de264df91c4302f5e14f8c63de2399
Roman Tiukh <frogour@gmail.com>
2020-07-03 22:19:37 +0300

    remove unused app

--- data_converter/settings.py
+++ data_converter/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
-    'rest_auth.registration',
+    # 'rest_auth.registration',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
