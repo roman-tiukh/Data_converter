75a7599b27895b7e0cd44e84e7df8a9d96a93f46
dev <pydev777@gmail.com>
2020-07-13 21:29:09 +0300

    Refactor rest_auth translate

--- data_converter/settings.py
+++ data_converter/settings.py
@@ -122,7 +122,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOCALE_PATHS = [os.path.join(BASE_DIR, "locale"), ]
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale"),
+    os.path.join(BASE_DIR, "data_converter/tpl/rest_auth", "locale"),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
