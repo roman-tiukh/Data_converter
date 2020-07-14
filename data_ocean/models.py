dd7a82fa1be05e7f65c3036970574be578295800
Iegor Soboliev <58389602+IegorSoboliev@users.noreply.github.com>
2020-07-14 16:38:50 +0300

    Added method urls to all registers (#209)
    * Added method urls to all registers
    
    * Added API methods to KOATUU register
    
    * Added API methods to KOATUU register
    
    * Added /

--- data_ocean/models.py
+++ data_ocean/models.py
@@ -29,6 +29,8 @@ class Register(DataOceanModel):
     name = models.CharField(max_length=500, unique=True)
     source_name = models.CharField(max_length=300)
     source_register_id = models.CharField(max_length=36, unique=True)
+    data_ocean_list = models.URLField(max_length=500)
+    data_ocean_retrieve = models.URLField(max_length=500)
     url_address = models.URLField(max_length=500)
     api_address = models.URLField(max_length=500, null=True)
     source_last_update = models.DateTimeField(default=None, null=True)
