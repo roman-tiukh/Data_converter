dafafe3c4be0283c0a14d7ab74930fbf195b8912
Iegor Soboliev <58389602+IegorSoboliev@users.noreply.github.com>
2020-08-14 10:41:54 +0300

    Koatuu api (#269)
    * Added filters, serializers, viewsets and url for every level of KOATUU objects
    
    * Added a migration

--- location_register/filters.py
+++ location_register/filters.py
@@ -1,6 +1,9 @@
 import django_filters
-from .models.ratu_models import RatuStreet
-from .models.drv_models import DrvBuilding
+
+from location_register.models.drv_models import DrvBuilding
+from location_register.models.koatuu_models import (KoatuuFirstLevel, KoatuuSecondLevel,
+                                                    KoatuuThirdLevel, KoatuuFourthLevel)
+from location_register.models.ratu_models import RatuStreet
 
 
 class RatuStreetFilterSet(django_filters.FilterSet):
@@ -16,4 +19,38 @@ class DrvBuildingFilterSet(django_filters.FilterSet):
 
     class Meta:
         model = DrvBuilding
+        fields = ()
+
+
+class KoatuuFirstLevelFilterSet(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = KoatuuFirstLevel
+        fields = ()
+
+
+class KoatuuSecondLevelFilterSet(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    category = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = KoatuuSecondLevel
+        fields = ()
+
+
+class KoatuuThirdLevelFilterSet(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    category = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = KoatuuThirdLevel
+        fields = ()
+
+
+class KoatuuFourthLevelFilterSet(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = KoatuuFourthLevel
         fields = ()
---
+++ location_register/views/koatuu_views.py
@@ -0,0 +1,54 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import AllowAny
+
+from data_ocean.views import CachedViewMixin
+from location_register.filters import (KoatuuFirstLevelFilterSet, KoatuuSecondLevelFilterSet,
+                                       KoatuuThirdLevelFilterSet, KoatuuFourthLevelFilterSet)
+from location_register.models.koatuu_models import (KoatuuFirstLevel, KoatuuSecondLevel,
+                                                    KoatuuThirdLevel, KoatuuFourthLevel)
+from location_register.serializers.koatuu_serializers import (KoatuuFirstLevelSerializer,
+                                                              KoatuuSecondLevelSerializer,
+                                                              KoatuuThirdLevelSerializer,
+                                                              KoatuuFourthLevelSerializer)
+
+
+class KoatuuFirstLevelViewSet(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = KoatuuFirstLevel.objects.prefetch_related('second_level_places').all()
+    serializer_class = KoatuuFirstLevelSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset = KoatuuFirstLevelFilterSet
+    search_fields = ('code', 'name')
+
+
+class KoatuuSecondLevelViewSet(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = (KoatuuSecondLevel.objects.select_related('first_level')
+                .prefetch_related('third_level_places')
+                .all())
+    serializer_class = KoatuuSecondLevelSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset = KoatuuSecondLevelFilterSet
+    search_fields = ('code', 'name')
+
+class KoatuuThirdLevelViewSet(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = (KoatuuThirdLevel.objects.select_related('first_level', 'second_level')
+                .prefetch_related('fourth_level_places')
+                .all())
+    serializer_class = KoatuuThirdLevelSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset = KoatuuThirdLevelFilterSet
+    search_fields = ('code', 'name')
+
+
+class KoatuuFourthLevelViewSet(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = KoatuuFourthLevel.objects.select_related(
+        'first_level', 'second_level', 'third_level', 'category').all()
+    serializer_class = KoatuuFourthLevelSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset = KoatuuFourthLevelFilterSet
+    search_fields = ('code', 'name')
