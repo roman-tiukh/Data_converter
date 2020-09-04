64464825a0564f70176db2becbbc14d4740765a8
IegorSoboliev <evsobolev@gmail.com>
2020-09-04 10:07:02 +0300

    Closed free access to th API

--- business_register/views/company_views.py
+++ business_register/views/company_views.py
@@ -1,17 +1,16 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.filters import SearchFilter
+
 from business_register.filters import CompanyFilterSet
 from business_register.models.company_models import Company, HistoricalCompany
 from business_register.serializers.company_and_pep_serializers import (CompanyListSerializer,
                                                                        CompanyDetailSerializer,
                                                                        HistoricalCompanySerializer)
 from data_ocean.views import CachedViewMixin
-from rest_framework.filters import SearchFilter
 
 
 class CompanyViewSet(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
     queryset = Company.objects.select_related(
         'parent', 'company_type', 'status', 'authority', 'bylaw', 'country',
     ).prefetch_related(
--- business_register/views/pep_views.py
+++ business_register/views/pep_views.py
@@ -1,15 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.filters import SearchFilter
+
 from business_register.filters import PepFilterSet
 from business_register.models.pep_models import Pep
 from business_register.serializers.company_and_pep_serializers import PepListSerializer, PepDetailSerializer
 from data_ocean.views import CachedViewMixin
-from rest_framework.filters import SearchFilter
 
 
 class PepViewSet(CachedViewMixin, viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
     queryset = Pep.objects.all()
     serializer_class = PepListSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
