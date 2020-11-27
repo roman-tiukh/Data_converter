03d0e9c38211827275224ae5cd770334fd1530e5
Iva-khar <ivakhar@gmail.com>
2020-11-27 14:06:53 +0200

    improve view user page and users list

--- users/admin.py
+++ users/admin.py
@@ -22,19 +22,21 @@ class DataOceanUserAdmin(UserAdmin):
     add_form = DataOceanUserCreationForm
     form = DataOceanUserChangeForm
     list_display = ('last_name', 'first_name', 'email', 'date_joined', 'last_login', 'date_of_birth', 'organization',
-                    'position', 'language', 'is_staff', 'is_active', 'is_superuser')
-    list_filter = ('last_name', 'first_name', 'email', 'date_joined', 'last_login', 'date_of_birth', 'organization',
-                   'position', 'language', 'is_staff', 'is_active', 'is_superuser')
+                    'language', 'is_staff', 'is_active')
+    list_filter = ('date_joined', 'last_login', 'date_of_birth', 'language', 'is_staff', 'is_active', 'is_superuser')
     fieldsets = (
-        (None, {'fields': ('last_name', 'first_name', 'email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        (None, {'fields': ('id', 'last_name', 'first_name', 'email', 'date_joined', 'last_login', 'date_of_birth',
+                           'organization', 'position', 'language', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
     )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
-         ),
-    )
-    search_fields = ('email',)
-    ordering = ('email',)
+    search_fields = ('last_name', 'first_name', 'email', 'organization')
+    ordering = ('date_joined',)
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
