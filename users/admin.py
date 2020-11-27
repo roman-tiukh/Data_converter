5d2afe19ee033f224195f6e852def3a184686a00
Iva-khar <ivakhar@gmail.com>
2020-11-27 16:02:03 +0200

    return add_fieldsets

--- users/admin.py
+++ users/admin.py
@@ -29,6 +29,12 @@ class DataOceanUserAdmin(UserAdmin):
                            'organization', 'position', 'language', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
     search_fields = ('last_name', 'first_name', 'email', 'organization')
     ordering = ('date_joined',)
 
