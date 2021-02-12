928ea571e641be1da15514e5f0ba3d07682d7031
Iva-khar <ivakhar@gmail.com>
2021-02-12 17:05:07 +0200

    fix2

--- data_ocean/downloader.py
+++ data_ocean/downloader.py
@@ -22,7 +22,6 @@ class Downloader(ABC):
     data = {}
     headers = {}
     local_path = settings.LOCAL_FOLDER
-    LOCAL_FILE_NAME_UO = settings.LOCAL_FILE_NAME_UO
     chunk_size = 8 * 1024 * 1024
     stream = True
     reg_name = ''
