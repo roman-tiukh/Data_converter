585cd5e3d280b50ca6c1e9c1099ed56719b8abe3
IegorSoboliev <evsobolev@gmail.com>
2021-02-22 14:08:46 +0200

    Update PEP filtering

--- business_register/filters.py
+++ business_register/filters.py
@@ -1,9 +1,10 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from business_register.models.company_models import Company
 from business_register.models.fop_models import Fop
-from business_register.models.pep_models import Pep
+from business_register.models.pep_models import Pep, CompanyLinkWithPep
 from data_ocean.filters import ValidatedBooleanWidget
 from .models.kved_models import Kved
 
@@ -75,15 +76,23 @@ class KvedFilterSet(filters.FilterSet):
 class PepFilterSet(filters.FilterSet):
     fullname = filters.CharFilter(lookup_expr='icontains')
     fullname_transcriptions_eng = filters.CharFilter(lookup_expr='icontains')
+    name_search = filters.CharFilter(label=_('Options for writing full name'),
+                                     method='filter_name_search')
     updated_at = filters.DateFromToRangeFilter()
     is_pep = filters.BooleanFilter(widget=ValidatedBooleanWidget)
     is_dead = filters.BooleanFilter(widget=ValidatedBooleanWidget)
-    name_search = filters.CharFilter(label='пошук по імені', method='filter_name_search')
+    pep_type = filters.ChoiceFilter(choices=Pep.TYPES)
+    related_company = filters.CharFilter(label=_("Associated company`s number (provide a number)"),
+                                         method='filter_related_company')
+    last_job_title = filters.CharFilter(lookup_expr='icontains')
+    last_employer = filters.CharFilter(lookup_expr='icontains')
 
     o = filters.OrderingFilter(
         fields=(
             ('fullname', 'fullname'),
+            ('fullname_and_dob', 'fullname_and_dob'),
             ('is_pep', 'is_pep'),
+            ('pep_type', 'pep_type'),
             ('pep_type', 'pep_type'),
             ('last_job_title', 'last_job_title'),
             ('last_employer', 'last_employer'),
@@ -95,6 +104,14 @@ class PepFilterSet(filters.FilterSet):
             Q(fullname__icontains=value) |
             Q(fullname_transcriptions_eng__icontains=value)
         )
+
+    def filter_related_company(self, queryset, value, company_number):
+        peps_id = CompanyLinkWithPep.objects.filter(
+            company__edrpou=company_number
+        ).values_list(
+            'pep_id', flat=True
+        )
+        return queryset.filter(id__in=peps_id)
 
     class Meta:
         model = Pep
