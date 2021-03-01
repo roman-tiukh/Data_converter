1c6154d1afec453268d4b450581d0c96791e0b13
Kostyantyn Logvynovsky <kostyantyn.logvynovsky@dataocean.us>
2021-03-01 17:16:36 +0200

    Merge branch 'master' of https://github.com/3v-workspace/Data_converter
    # Conflicts:
    #	business_register/serializers/company_and_pep_serializers.py

--- location_register/models/ratu_models.py
+++ location_register/models/ratu_models.py
@@ -6,20 +6,19 @@ from location_register.models.koatuu_models import KoatuuCategory
 
 
 class RatuRegion(DataOceanModel):
-    name = models.CharField(_('name'), max_length=30, help_text='Name of the region', unique=True)
-    koatuu = models.CharField('code KOATUU', max_length=10, help_text='Code of the region, according to the state'
-                                                                      ' classifier of objects of administrative-territorial'
-                                                                      ' organization of Ukraine',
-                                                                      unique=True, null=True)
+    name = models.CharField(_('name'), max_length=30, unique=True, help_text='Name of the region')
+    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the region, according to the state'
+                                                                      ' Classifier of objects of administrative-territorial'
+                                                                      ' organization of Ukraine')
 
     class Meta:
         verbose_name = _('region')
 
 class RatuDistrict(DataOceanModel):
     region = models.ForeignKey(RatuRegion, on_delete=models.CASCADE, verbose_name='region')
-    name = models.CharField(_('name'), help_text='Name of the district', max_length=100)
-    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the region, according to the state'
-                                                                      ' classifier of objects of administrative-territorial'
+    name = models.CharField(_('name'), max_length=100, help_text='Name of the district')
+    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the district, according to the state'
+                                                                      ' Classifier of objects of administrative-territorial'
                                                                       ' organization of Ukraine')
     code = models.CharField('code', max_length=200)
 
@@ -34,8 +33,8 @@ class RatuCity(DataOceanModel):
     category = models.ForeignKey(KoatuuCategory, on_delete=models.CASCADE, null=True,
                                  verbose_name='category')
     name = models.CharField(_('name'), max_length=100, help_text='City name')
-    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the region, according to the state'
-                                                                      ' classifier of objects of administrative-territorial'
+    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the city, according to the state'
+                                                                      ' Classifier of objects of administrative-territorial'
                                                                       ' organization of Ukraine')
     code = models.CharField('code', max_length=200)
 
@@ -51,9 +50,9 @@ class RatuCityDistrict(DataOceanModel):
                              verbose_name='city')
     category = models.ForeignKey(KoatuuCategory, on_delete=models.CASCADE, null=True,
                                  verbose_name='category')
-    name = models.CharField(_('name'), help_text='Name of the city district', max_length=100)
-    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the region, according to the state'
-                                                                      ' classifier of objects of administrative-territorial'
+    name = models.CharField(_('name'), max_length=100, help_text='Name of the city district')
+    koatuu = models.CharField('code KOATUU', max_length=10, unique=True, null=True, help_text='Code of the city district, according to the state'
+                                                                      ' Classifier of objects of administrative-territorial'
                                                                       ' organization of Ukraine')
     code = models.CharField('code', max_length=200)
 
@@ -69,7 +68,7 @@ class RatuStreet(DataOceanModel):
                              verbose_name=_('city'))
     citydistrict = models.ForeignKey(RatuCityDistrict, on_delete=models.CASCADE, null=True,
                                      verbose_name=_('district of the city'))
-    name = models.CharField(_('name'),help_text='City name', max_length=100)
+    name = models.CharField(_('name'), max_length=100, help_text='Street name')
     code = models.CharField(_('code'), max_length=200)
 
     class Meta:
