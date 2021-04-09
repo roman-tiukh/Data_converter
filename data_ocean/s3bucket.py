8fa2b1350757250a14d241d3158d955e15e848dd
pydev777 <pydev777@gmail.com>
2021-04-09 17:31:42 +0300

    Save file to public S3

--- data_ocean/s3bucket.py
+++ data_ocean/s3bucket.py
@@ -2,7 +2,6 @@ import logging
 import string
 import random
 import pathlib
-from datetime import timedelta
 from django.utils import timezone
 import boto3
 from django.conf import settings
@@ -11,74 +10,97 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def save_file(file_path='', file_body='', content_disposition: str = 'attachment',
-              acl: str = 'public-read', expires_days: int = 7) -> str:
+PUBLIC_READ = 'public-read'
+PRIVATE = 'private'
+ACLS = (
+    PUBLIC_READ,  # public access
+    PRIVATE,      # private access
+)
 
-    acls = ('public-read', 'private')
-    content_dispositions = ('inline', 'attachment')
-    content_types = {
-        '.csv': 'text/csv',
-        '.doc': 'application/msword',
-        '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        '.json': 'application/json',
-        '.html': 'text/html',
-        '.pdf': 'application/pdf',
-        '.txt': 'text/plain',
-        '.xls': 'application/vnd.ms-excel',
-        '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.xlsx',
-        '.xml': 'application/xml',
-        '.zip': 'application/zip',
-    }
-    bucket_name = settings.AWS_S3_BUCKET_NAME
-    bucket_region = settings.AWS_S3_REGION_NAME
-    access_key_id = settings.AWS_S3_ACCESS_KEY_ID
-    secret_access_key = settings.AWS_S3_SECRET_ACCESS_KEY
-    hash_length = 16
+INLINE = 'inline'
+ATTACHMENT = 'attachment'
+CONTENT_DISPOSITION = (
+    INLINE,       # open file in browser window
+    ATTACHMENT,   # download file
+)
 
-    if acl not in acls:
-        acl = 'public-read'
+CONTENT_TYPES = {
+    '.csv': 'text/csv',
+    '.doc': 'application/msword',
+    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    '.json': 'application/json',
+    '.html': 'text/html',
+    '.pdf': 'application/pdf',
+    '.txt': 'text/plain',
+    '.xls': 'application/vnd.ms-excel',
+    '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.xlsx',
+    '.xml': 'application/xml',
+    '.zip': 'application/zip',
+}
 
-    if content_disposition not in content_dispositions:
-        content_disposition = 'attachment'
+BUCKET_NAME = settings.AWS_S3_BUCKET_NAME
+BUCKET_REGION = settings.AWS_S3_REGION_NAME
+ACCESS_KEY_ID = settings.AWS_S3_ACCESS_KEY_ID
+SECRET_ACCESS_KEY = settings.AWS_S3_SECRET_ACCESS_KEY
+HASH_LENGTH = 16
 
-    file_path = str(file_path).strip('/')
-    if not file_path:
-        raise Exception('File path cannot be empty!')
 
-    # insert hash to filepath
-    file_path_suffix = pathlib.Path(file_path).suffix
-    file_path_wo_suffix = file_path[:-len(file_path_suffix)]
-    hash = ''.join(random.choices(string.ascii_letters + string.digits, k=hash_length))
-    file_path_hashed = f'{file_path_wo_suffix}_{hash}{file_path_suffix}'
+def save_file(
+        file_path: str,
+        file_body,
+        content_disposition: str = ATTACHMENT,
+        acl: str = PUBLIC_READ,
+        hashing: bool = True,
+) -> str:
+
+    if content_disposition not in CONTENT_DISPOSITION:
+        content_disposition = ATTACHMENT
+
+    if acl not in ACLS:
+        acl = PUBLIC_READ
+
+    file_path_str = str(file_path).strip('/')
+    if not file_path_str:
+        raise ValueError('File path cannot be empty!')
+
+    file_path = pathlib.Path(file_path_str)
+
+    # add hash to file name
+    if hashing:
+        f_hash = ''.join(random.choices(string.ascii_letters + string.digits, k=HASH_LENGTH))
+        file_name_hashed = f'{file_path.stem}_{f_hash}{file_path.suffix}'
+        if str(file_path.parent) == '.':
+            file_path_str = file_name_hashed
+        else:
+            file_path_str = f'{file_path.parent}/{file_name_hashed}'
 
     # auto ContentType
-    if file_path_suffix in content_types:
-        content_type = content_types[file_path_suffix]
+    if file_path.suffix in CONTENT_TYPES:
+        content_type = CONTENT_TYPES[file_path.suffix]
     else:
         content_type = 'application/octet-stream',
-        content_disposition = 'attachment'
+        content_disposition = ATTACHMENT
 
     s3 = boto3.resource(
         's3',
-        aws_access_key_id=access_key_id,
-        aws_secret_access_key=secret_access_key,
-        region_name=bucket_region,
+        aws_access_key_id=ACCESS_KEY_ID,
+        aws_secret_access_key=SECRET_ACCESS_KEY,
+        region_name=BUCKET_REGION,
     )
-    if s3.Bucket(bucket_name).creation_date is None:
-        raise Exception('This bucket does not exist!')
+    if s3.Bucket(BUCKET_NAME).creation_date is None:
+        raise ValueError('This bucket does not exist!')
 
-    logger.info(f'[{timezone.now()}] s3bucket save file: {file_path_hashed} ...')
+    logger.info(f'[{timezone.now()}] s3bucket save file: {file_path} ...')
 
-    s3.Bucket(bucket_name).put_object(
-        Key=file_path_hashed,
+    s3.Bucket(BUCKET_NAME).put_object(
+        Key=file_path_str,
         Body=file_body,
         ACL=acl,
         ContentDisposition=content_disposition,
         ContentType=content_type,
-        Expires=timezone.now()+timedelta(days=expires_days),
     )
 
-    url = f'https://{bucket_name}.s3.{bucket_region}.amazonaws.com/{file_path_hashed}'
+    url = f'https://{BUCKET_NAME}.s3.{BUCKET_REGION}.amazonaws.com/{file_path_str}'
     logger.warning(f'[{timezone.now()}] s3bucket file URL: {url}')
 
     return url
