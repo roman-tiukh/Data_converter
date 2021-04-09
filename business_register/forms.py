78ae81e9b76c3d335baa7a7e1962eba496b9b785
Roman Tiukh <frogour@gmail.com>
2021-04-09 10:40:51 +0300

    [DO-1585] pep_check by transcription

--- business_register/forms.py
+++ business_register/forms.py
@@ -1,3 +1,4 @@
+import re
 from datetime import timedelta
 from django import forms
 from django.utils.translation import ugettext_lazy as _
@@ -18,3 +19,33 @@ class PepExportForm(forms.Form):
             })
         else:
             return cleaned_data
+
+
+class PepCheckFilterForm(forms.Form):
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        middle_name = cleaned_data.get('middle_name')
+        fullname_transcription = cleaned_data.get('fullname_transcription')
+
+        transcription_or_ukr_names_error = forms.ValidationError(_(
+            'Use first_name + last_name + middle_name '
+            'or fullname_transcription fields for check person'
+        ))
+        if first_name or last_name or middle_name:
+            if fullname_transcription:
+                raise transcription_or_ukr_names_error
+            if not first_name:
+                raise forms.ValidationError({'first_name': _('This field is required')})
+            if not last_name:
+                raise forms.ValidationError({'last_name': _('This field is required')})
+        elif fullname_transcription:
+            fullname_transcription = re.sub(r'\s{2,}', ' ', fullname_transcription)
+            if len(fullname_transcription.split(' ')) < 2:
+                raise forms.ValidationError({
+                    'fullname_transcription': _('The length of the field value must be 2 or more words')
+                })
+        else:
+            raise transcription_or_ukr_names_error
+        return cleaned_data
