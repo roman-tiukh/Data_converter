fac3fa48635a5c2109f92aa3bc55e5cff10f1b2c
Roman Tiukh <frogour@gmail.com>
2021-04-27 18:59:18 +0300

    update load_person_sanctions command

--- business_register/management/commands/load_person_sanctions.py
+++ business_register/management/commands/load_person_sanctions.py
@@ -8,46 +8,56 @@ class Command(BaseCommand):
     help = 'Load person sanctions'
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('-s', '--start-row', type=int, nargs='?', default=1)
 
     def handle(self, *args, **options):
-        wb = load_workbook('./source_data/data_for_upload/normalized_pers_sanc_14_05 _FULL (copy).xlsx')
+        start_row = options['start_row']
+        wb = load_workbook('./source_data/data_for_upload/normalized_pers_sanc_28_04_2017.xlsx')
         reasoning = (
-            'рішення Ради національної безпеки і оборони України від 14 травня 2020 року '
-            '"Про застосування, скасування і внесення змін до персональних спеціальних економічних '
-            'та інших обмежувальних заходів (санкцій)'
+            'рішення Ради національної безпеки і оборони України від 28 квітня 2017 року '
+            '"Про застосування персональних спеціальних економічних та '
+            'інших обмежувальних заходів (санкцій)'
         )
         ws = wb['Sheet']
         for i, row in enumerate(ws):
-            self.stdout.write(f'Process row #{i + 1}', ending='\r')
-            if i == 0:
+            if i < start_row:
                 continue
+            self.stdout.write(f'\r Process row #{i}', ending='')
             row = [cell.value for cell in row]
             person = PersonSanction.objects.create(
                 first_name=row[2],
                 last_name=row[3],
                 middle_name=row[4] or '',
                 full_name=f'{row[3]} {row[2]} {row[4]}',
-                full_name_original_transcription=row[6] or '',
-                date_of_birth=row[7],
+                place_of_birth=row[6] or '',
+                id_card=row[8] or '',
+                taxpayer_number=row[9] or '',
+                address=row[10] or '',
+                occupation=row[11] or '',
+                full_name_original_transcription=row[12] or '',
+                date_of_birth=row[13],
+                end_date=row[15],
+                start_date=row[17],
                 reasoning=reasoning,
-                end_date=row[9],
-                start_date=row[11],
-                place_of_birth=row[12] or '',
-                address=row[13] or '',
-                id_card=row[14] or '',
             )
 
             for country in row[5].split('&&'):
                 country = country.strip().lower()
-                person.countries_of_citizenship.add(Country.objects.get(name=country))
+                try:
+                    person.countries_of_citizenship.add(Country.objects.get(name=country))
+                except Country.DoesNotExist:
+                    self.stdout.write(f'\n Країну не знайдено: {country}')
+                    exit(1)
 
-            for sanction_type_name in row[8].split('&&'):
+            for sanction_type_name in row[14].split('&&'):
                 sanction_type_name = sanction_type_name.strip()
                 sanction_type, created = SanctionType.objects.get_or_create(
                     name=sanction_type_name,
                     defaults={'law': 'про санкції'},
                 )
+                if created:
+                    self.stdout.write(f'\n Створено новий тип санкції: {sanction_type_name}')
                 person.types_of_sanctions.add(sanction_type)
 
             person.save()
+        self.stdout.write('\n Done')
