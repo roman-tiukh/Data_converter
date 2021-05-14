f70186863477227460883dfde33a50a1c09f4886
pydev777 <pydev777@gmail.com>
2021-05-14 18:04:03 +0300

    Fix download chunk size

--- data_ocean/downloader.py
+++ data_ocean/downloader.py
@@ -25,7 +25,7 @@ class Downloader(ABC):
     data = {}
     headers = {}
     local_path = settings.LOCAL_FOLDER
-    chunk_size = settings.DOWNLOADER_CHUNK_SIZE
+    chunk_size = 16 * 1024
     stream = True
     reg_name = ''
     file_name = ''
