82cb9af0fd13b9f2cc6f559efb4d4022028d2055
EmmaSamoilova <emmasamojlova@gamil.com>
2021-05-17 15:34:23 +0300

    changed name_ukr to name_uk

--- location_register/models/address_models.py
+++ location_register/models/address_models.py
@@ -5,8 +5,8 @@ from data_ocean.models import DataOceanModel
 
 
 class Country(DataOceanModel):
-    name = models.CharField(_('name'), max_length=60, unique=True)
-    name_ukr = models.CharField(_('ukrainian name'), max_length=100, default='')
+    name = models.CharField(_('english name'), max_length=60, unique=True)
+    name_uk = models.CharField(_('ukrainian name'), max_length=100, default='')
 
     class Meta:
         verbose_name = _('country')
