6303a5deaad129a3b1ba94d1b9c9030b7ae9164e
Iva-khar <ivakhar@gmail.com>
2021-05-27 14:15:26 +0300

    add not working converters to report

--- data_ocean/emails.py
+++ data_ocean/emails.py
@@ -2,17 +2,23 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from data_converter.email_utils import send_template_mail
-from data_ocean.models import Report
+from data_ocean.models import Register, Report
 from users.models import DataOceanUser
 
 
 def send_reports_mail():
+    reports = Report.collect_last_day_reports()
+    not_working = []
+    for register in list(Register.objects.exclude(in_daily_report=None).values('in_daily_report')):
+        if all(register['in_daily_report'] != report.registry_name for report in reports):
+            not_working.append(register['in_daily_report'])
     send_template_mail(
-        to=settings.REPORT_EMAILS,
+        to=['ivakhar@gmail.com'],
         subject='Звіт про оновлення реєстрів за останні 24 години',
         template='data_ocean/emails/report.html',
         context={
-            'reports': Report.collect_last_day_reports()
+            'reports': reports,
+            'not_working': not_working
         }
     )
 
