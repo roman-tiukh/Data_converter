ce756bdfd46adece6060e5207a6c4df3d067ed8f
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-06-01 16:10:10 +0300

    Update pep.py
    Creating few variables

--- business_register/converter/pep.py
+++ business_register/converter/pep.py
@@ -94,8 +94,10 @@ class PepConverterFromJson(BusinessConverter):
             middle_name = pep_dict['patronymic'].lower()
             last_name = pep_dict['last_name'].lower()
             fullname = pep_dict['full_name'].lower()
-            fullname_en = pep_dict['last_name_en'].lower() + ' ' + pep_dict['first_name_en'].lower() + ' ' + \
-                          pep_dict['patronymic_en'].lower()
+            first_name_en = pep_dict['first_name_en'].lower()
+            middle_name_en = pep_dict['patronymic_en'].lower()
+            last_name_en = pep_dict['last_name_en'].lower()
+            fullname_en = last_name_en + ' ' + first_name_en + ' ' + middle_name_en
             fullname_transcriptions_eng = pep_dict['names'].lower()
             last_job_title = pep_dict.get('last_job_title')
             if last_job_title:
