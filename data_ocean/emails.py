b54f50f16515a64ed239fe3a9a580624e9a7e52b
Iva-khar <ivakhar@gmail.com>
2021-06-01 20:51:59 +0300

    after review changes

--- data_ocean/emails.py
+++ data_ocean/emails.py
@@ -8,12 +8,11 @@ from users.models import DataOceanUser
 
 def send_reports_mail():
     reports = Report.collect_last_day_reports()
-    not_updated_registers = []
-    for register in list(Register.objects.exclude(name_in_daily_report='').values('name_in_daily_report')):
-        if all(register['name_in_daily_report'] != report.registry_name for report in reports):
-            not_updated_registers.append(register['name_in_daily_report'])
+    not_updated_registers = Register.objects.exclude(
+        name_in_daily_report__in=[report.registry_name for report in reports]
+    ).exclude(name_in_daily_report='')
     send_template_mail(
-        to=settings.REPORT_EMAILS,
+        to=['ivakhar@gmail.com'],
         subject='Звіт про оновлення реєстрів за останні 24 години',
         template='data_ocean/emails/report.html',
         context={
