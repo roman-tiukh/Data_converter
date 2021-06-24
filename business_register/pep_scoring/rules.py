1dbcd299726d991e220f92544c3f3948f55aad9c
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-06-24 17:18:30 +0300

    Create rules.py
    class BaseScoringRule + PEP03_* rules

---
+++ business_register/pep_scoring/rules.py
@@ -0,0 +1,105 @@
+from abc import ABC, abstractmethod
+import json
+from business_register.models.declaration_models import (Declaration,
+                                                         Property,
+                                                         Vehicle,
+                                                         VehicleRight,
+                                                         Income,
+                                                         Money,
+                                                         )
+from business_register.models.pep_models import (RelatedPersonsLink, Pep)
+from location_register.models.ratu_models import RatuCity
+
+
+class BaseScoringRule(ABC):
+    def __init__(self, pep):
+        self.pep = pep
+
+    @abstractmethod
+    def calculate_weight(self):
+        pass
+
+
+class IsRealEstateWithoutValue(BaseScoringRule):
+    """
+    Rule 3.1 - PEP03_home
+    weight - 0.4
+    There is no information on the value of the real estate owned by PEP or
+    family members since 2015
+    """
+
+    def calculate_weight(self):
+        family_ids = self.pep.related_persons.filter(
+            to_person_links__category=RelatedPersonsLink.FAMILY,
+        ).values_list('id', flat=True).all()[::1]
+        family_ids.append(self.pep.id)
+        have_weight = Property.objects.filter(
+            declaration__pep_id__in=family_ids,
+            valuation__isnull=True,
+            type=Property.SUMMER_HOUSE,
+            created_at__year__gte=2015,
+        ).values_list('id', 'declaration_id').all()[::1]
+        if have_weight:
+            value = {
+                "weight": 0.4,
+                "property_id": have_weight[0][0],
+                "declaration_id": have_weight[0][1],
+            }
+            return json.dumps(value)
+        return 0
+
+
+class IsLandWithoutValue(BaseScoringRule):
+    """
+    Rule 3.2 - PEP03_land
+    weight - 0.1
+    There is no information on the value of the land owned by PEP or
+    family members since 2015
+    """
+
+    def calculate_weight(self):
+        family_ids = self.pep.related_persons.filter(
+            to_person_links__category=RelatedPersonsLink.FAMILY,
+        ).values_list('id', flat=True).all()[::1]
+        family_ids.append(self.pep.id)
+        have_weight = Property.objects.filter(
+            declaration__pep_id__in=family_ids,
+            valuation__isnull=True,
+            type=Property.LAND,
+            created_at__year__gte=2015,
+        ).values_list('id', 'declaration_id').all()[::1]
+        if have_weight:
+            value = {
+                "weight": 0.1,
+                "property_id": have_weight[0][0],
+                "declaration_id": have_weight[0][1],
+            }
+            return json.dumps(value)
+        return 0
+
+
+class IsAutoWithoutValue(BaseScoringRule):
+    """
+    Rule 3.3 - PEP03_car
+    weight - 0.4
+    There is no information on the value of the vehicle owned by PEP or
+    family members since 2015
+    """
+
+    def calculate_weight(self):
+        family_ids = self.pep.related_persons.filter(
+            to_person_links__category=RelatedPersonsLink.FAMILY,
+        ).values_list('id', flat=True).all()[::1]
+        family_ids.append(self.pep.id)
+        have_weight = Vehicle.objects.filter(
+            declaration__pep_id__in=family_ids,
+            created_at__year__gte=2015,
+        ).values_list('id', 'declaration_id').all()[::1]
+        if have_weight:
+            value = {
+                "weight": 0.4,
+                "property_id": have_weight[0][0],
+                "declaration_id": have_weight[0][1],
+            }
+            return json.dumps(value)
+        return 0
