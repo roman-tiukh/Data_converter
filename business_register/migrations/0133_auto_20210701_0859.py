e7ed45e1f930ad541f90086b2fbd7d9ccc3bd516
Roman Tiukh <frogour@gmail.com>
2021-07-01 17:41:34 +0300

    fix migrations 2

--- business_register/migrations/0133_auto_20210701_0859.py
+++ business_register/migrations/0133_auto_20210701_0859.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('business_register', '0132_merge_20210701_0805'),
+        ('business_register', '0132_merge_20210630_1537'),
     ]
 
     operations = [
