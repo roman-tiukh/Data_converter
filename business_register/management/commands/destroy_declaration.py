e1aee7bee5f78454d6dee1bf6b30ccac001fd0c2
Roman Tiukh <frogour@gmail.com>
2021-07-05 17:02:31 +0300

    destroy_declaration management command

---
+++ business_register/management/commands/destroy_declaration.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from business_register.models.declaration_models import Declaration
+
+
+class Command(BaseCommand):
+    help = '---'
+
+    def add_arguments(self, parser):
+        parser.add_argument('declaration_id', type=str, nargs=1)
+
+    def handle(self, *args, **options):
+        declaration_id = options['declaration_id'][0]
+        if declaration_id.isdigit():
+            declaration_id = int(declaration_id)
+            declaration = Declaration.objects.get(id=declaration_id)
+        else:
+            declaration = Declaration.objects.get(nacp_declaration_id=declaration_id)
+        declaration.destroy()
